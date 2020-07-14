fb2d486b101420a20f89ca1fa829af7e90a29f0e
Cat <ccallcottstevens@gmail.com>
2020-07-14 19:45:56 +1000

    added env files

--- cypress/integration/users/loginLogout.spec.js
+++ cypress/integration/users/loginLogout.spec.js
@@ -8,7 +8,7 @@ before(() => {
 
 beforeEach(() => {
     cy.viewport(1024, 768)
-    // cy.visit('/')
+    cy.visit('/')
     cy.get('[data-cy=navbar]').then((nav) => {
         if(nav.find('[data-cy=logout]').length > 0) {
             cy.get('[data-cy=logout]').click()
